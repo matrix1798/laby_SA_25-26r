ecb6c393d1018ea48d9edb5bdd9b3fa8671fb27c
Matrix <mateuszkuczerowski832@gmail.com>
2025-10-21 19:46:07 +0200

    Dodanie symulacji dla ukladu B

--- laby_2/symulation/lab2_sym.m
+++ laby_2/symulation/lab2_sym.m
@@ -108,7 +108,7 @@ if false
         w = wout / (2*pi); % Hz
         
         % --- Dane pomiarowe (dla danej iteracji) ---
-        f = f_pom{i};
+        f = f_pom{i}*2*pi;
         Ain = A_in{i};
         Aout = A_out{i};
         phi = phi_pom{i};
@@ -152,7 +152,7 @@ if false
 end
 
 %bode bez zaznaczania punktów
-if true 
+if false 
     for i = 1 : 3
         figure(i); 
         Gc = tf(K_c(i)); 
@@ -163,7 +163,7 @@ if true
 end
 
 % Wykres Nequista: dla ukladu otwartego z regulatorem P, 3 wykresy
-if false
+if true
     for i=1 : 3
         figure(i);
         Gc = tf(K_c(i));
@@ -178,16 +178,6 @@ end
 
 %% Uklad B
 
-%pomiary z oscylokopu dla k1
-t_pomiar_k1 = [ 0.002, 0.003, 0.004, 0.005, 0.007];
-y_pomiar_k1 = [  1.2,   1.85,  2.1,   2.0,   1.8];
-%pomiary z oscylokopu dla k2
-t_pomiar_k2 = [ 0.002, 0.003, 0.004, 0.005, 0.007];
-y_pomiar_k2 = [  1.2,   1.85,  2.1,   2.0,   1.8];
-%pomiary z oscylokopu dla k3
-t_pomiar_k3 = [ 0.002, 0.003, 0.004, 0.005, 0.007];
-y_pomiar_k3 = [  1.2,   1.85,  2.1,   2.0,   1.8];
-
 K_c = [0.47,1,2.1];
 
 T_i = 0.0013;
@@ -198,7 +188,17 @@ T_y = 0.0001;
 Go = tf([-T_x 1],[T_y 1]);
 
 %symulacja
-if true
+if false
+    %pomiary z oscylokopu dla k1
+    t_pomiar_k1 = [ 0.002, 0.003, 0.004, 0.005, 0.007];
+    y_pomiar_k1 = [  1.2,   1.85,  2.1,   2.0,   1.8];
+    %pomiary z oscylokopu dla k2
+    t_pomiar_k2 = [ 0.002, 0.003, 0.004, 0.005, 0.007];
+    y_pomiar_k2 = [  1.2,   1.85,  2.1,   2.0,   1.8];
+    %pomiary z oscylokopu dla k3
+    t_pomiar_k3 = [ 0.002, 0.003, 0.004, 0.005, 0.007];
+    y_pomiar_k3 = [  1.2,   1.85,  2.1,   2.0,   1.8];
+
     t = 0:0.0001:0.01;
     
     u = zeros(size(t));
@@ -244,7 +244,98 @@ if true
     xlabel('Czas [s]');
     ylabel('Odpowiedź wyjściowa');
     legend(legend_entries); % Stwórz legendę na podstawie zebranych etykiet
+end
 
+% Char. Bodego: dla ukladu zamknietego (bo takie mamy pomiary), trzy
+% charakterystyki
+if true
+    f_pom = { [1 10 100], [1 5 50 100], [1 10 100 1000] };     % Hz
+    A_in  = { [1 1 1], [1 1 1 1], [1 1 1 1] };
+    A_out = { [0.9 0.7 0.3], [0.95 0.8 0.4 0.2], [1 0.9 0.5 0.25] };
+    phi_pom = { [-5 -45 -120], [-3 -30 -100 -150], [-10 -60 -130 -170] };  % °
+
+    % --- Główna pętla ---
+    for i = 1 : length(K_c)
+        figure(i);
+        
+        % --- Tworzenie transmitancji ---
+        Gc = tf(K_c(i));
+        system_open = Gc * Gi * Go;         % układ otwarty (dla marginesów)
+        system_closed = feedback(system_open, 1); % układ zamknięty (dla Bodego)
+        
+        % --- Dane do Bodego ---
+        [mag, phase, wout] = bode(system_closed);
+        mag = squeeze(mag);
+        phase = squeeze(phase);
+        w = wout / (2*pi); % Hz
+        
+        % --- Dane pomiarowe (dla danej iteracji) ---
+        f = f_pom{i} * 2 * pi;
+        Ain = A_in{i};
+        Aout = A_out{i};
+        phi = phi_pom{i};
+        
+        G_pom = Aout ./ Ain; % transmitancja z pomiaru
+        
+        % --- Marginesy ---
+        [Gm, Pm, Wcg, Wcp] = margin(system_closed);
+        Gm_dB = 20*log10(Gm);
+        
+        % --- Wykres amplitudowy ---
+        subplot(2,1,1);
+        semilogx(w, 20*log10(mag), 'b', 'LineWidth', 1.5); hold on;
+        semilogx(f, 20*log10(G_pom), 'ro', 'MarkerFaceColor', 'r');
+        grid on;
+        ylabel('Amplituda [dB]');
+        title(['Charakterystyka Bodego - K_c = ', num2str(K_c(i))]);
+        legend('Model', 'Pomiar', 'Location', 'Best');
+        
+        % --- Zaznaczenie zapasu wzmocnienia ---
+        if ~isnan(Wcg)
+            semilogx(Wcg/(2*pi), 0, 'ks', 'MarkerFaceColor', 'y', 'DisplayName', 'Zapas wzmocnienia');
+            text(Wcg/(2*pi), 3, sprintf('Gm = %.1f dB', Gm_dB), 'HorizontalAlignment', 'center');
+        end
+        
+        % --- Wykres fazowy ---
+        subplot(2,1,2);
+        semilogx(w, phase, 'b', 'LineWidth', 1.5); hold on;
+        semilogx(f, phi, 'ro', 'MarkerFaceColor', 'r');
+        grid on;
+        ylabel('Faza [°]');
+        xlabel('Częstotliwość [Hz]');
+        legend('Model', 'Pomiar', 'Location', 'Best');
+        
+        % --- Zaznaczenie zapasu fazy ---
+        if ~isnan(Wcp)
+            semilogx(Wcp/(2*pi), -180, 'ks', 'MarkerFaceColor', 'c', 'DisplayName', 'Zapas fazy');
+            text(Wcp/(2*pi), -165, sprintf('Pm = %.1f°', Pm), 'HorizontalAlignment', 'center');
+        end
+    end
+end
+
+%bode bez zaznaczania punktów
+if false 
+    for i = 1 : 3
+        figure(i); 
+        Gc = tf(K_c(i)); 
+        system_closed = feedback(Gc*Gi*Go,1);
+        margin(system_closed); %zaznacza odrazu zapas wzmocnienia 
+        legend(['Kc = ',num2str(K_c(i))]);
+    end
+end
+
+% Wykres Nequista: dla ukladu otwartego z regulatorem P, 3 wykresy
+if false
+    for i=1 : 3
+        figure(i);
+        Gc = tf(K_c(i));
+        nyquist(Gc*Go*Gi);
+        xlim([-1.4,0.1]);
+        ylim([-3,3]);
+        grid on;
+        legend(['K_c = ', num2str(K_c(i))]);
+    end 
+    %title('Charakterystyka Nequista ukladu A')
 end
 
 %% uklad D
